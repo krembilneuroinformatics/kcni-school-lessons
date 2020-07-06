fc874ec8419d1de341f466499986df68e8f4a7ec
John Griffiths <j.davidgriffiths@gmail.com>
2020-07-06 08:38:28 -0400

    day 1 content

---
+++ day1/source_this_if_plink_not_found.sh
@@ -0,0 +1,7 @@
+export PLINK_VERSION="150314"
+export PLINK_HOME=/usr/local/plink
+export PATH=$PLINK_HOME:$PATH
+
+export PRSice_VERSION="2.3.1.e"
+export PRSice_HOME=/usr/local/pipeline
+export PATH=$PRSice_HOME:$PATH
