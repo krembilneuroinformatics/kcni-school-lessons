db5b1ed6737dc399761fe9346b8550644f71f682
Erin W Dickie <erin.w.dickie@gmail.com>
2021-07-02 23:33:02 -0400

    trying r all things binder

---
+++ .binder/install.R
@@ -0,0 +1,22 @@
+install.packages("tidyverse")
+install.packages("rmarkdown")
+
+install.packages("ggrepel") 
+install.packages("ggthemes")
+install.packages("here")
+
+install.packages("caret")
+install.packages("glmnet")
+install.packages("fairness") 
+
+install.packages("Seurat")
+install.packages("BiocManager")
+BiocManager::install("MAST")
+BiocManager::install("edgeR")
+devtools::install_github("oganm/markerGeneProfile", force = T)
+install.packages("ggpubr")
+
+install.packages("Hmisc")
+install.packages("rms")
+install.packages("mice")
+install.packages("SNFtool")
