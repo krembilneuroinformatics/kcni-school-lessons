f023753be0babcb1d84c5b5be2356f8f703f25fe
murdugan <daniel.hauke@gmx.net>
2021-07-09 13:37:17 -0400

    removed all unnecessary scripts

--- day6/Toolboxes/tnueeg/analysis_steps/other/tnueeg_save_spm_template_fiducial_locations.m
+++
@@ -1,9 +0,0 @@
-function fid = tnueeg_save_spm_template_fiducial_locations(filename)
-
-fid.labels = {'NAS'; 'LPA'; 'RPA'};
-fid.data   = [  1,    85,    -41; ...
-              -83,   -20,    -65; ...
-               83,   -20,    -65];
-save(filename, 'fid');
-
-end
--- day6/Toolboxes/tnueeg/examples/dprst_mmn/code/steps/sub_preproc/dprst_strip_sfp_off_headshape.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for fileName in *.sfp; do 
-	cp $fileName tmp.sfp
-	sed '/^ *$/d' tmp.sfp | head -n 67 > $fileName
-done
