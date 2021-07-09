6b840ed805e824323019455f6b4d09e87f8b147b
murdugan <daniel.hauke@gmx.net>
2021-07-09 13:38:00 -0400

    organized kcni_setup_paths and removed dmpad_setup_spm function from script

--- day6/kcni_setup_paths.m
+++ day6/kcni_setup_paths.m
@@ -1,45 +1,30 @@
 function output = kcni_setup_paths()
-% restores default paths, add project paths including SPM (but without
-% sub-folders), sets up batch editor
-%
-%   output = kcni_setup_paths(input)
-%
-% IN
-%
-% OUT
+% -------------------------------------------------------------------------
+% Restores default paths, add project paths including SPM (but without
+% sub-folders), sets up batch editor.
 %
 % EXAMPLE
 %   kcni_setup_paths
 %
-%   See also
-%
 % Author:   Andreea Diaconescu
 % Created:  2020-06-01
 % Copyright (C) 2019 KCNI
+% -------------------------------------------------------------------------
 
-% remove all other toolboxes
-restoredefaultpath;
 
-pathProject = fileparts(mfilename('fullpath'));
-
-% remove all other toolboxes
-restoredefaultpath;
-
+%% Main
+restoredefaultpath; % remove all other toolboxes
+pathProject = fileparts(mfilename('fullpath')); % save path to project
 warning off; % to remove obvious path warnings for now
-
-% add project path with all sub-paths
-addpath(genpath(pathProject));
+addpath(genpath(pathProject)); % add project path with all sub-paths
 
 
-%% remove SPM subfolder paths 
+%% Remove SPM subfolders from path 
 % NOTE: NEVER add SPM with subfolders to your path, since it creates
 % conflicts with Matlab core functions, e.g., uint16
-
-pathSpm = fileparts(which('spm'));
-% remove subfolders of SPM, since it is recommended,
+% We will remove subfolders of SPM, since it is recommended,
 % and fieldtrip creates conflicts with Matlab functions otherwise
+pathSpm = fileparts(which('spm'));
 rmpath(genpath(pathSpm));
 addpath(pathSpm);
-dmpad_setup_spm();
-
 warning on
